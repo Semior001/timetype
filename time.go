e8ea2d24b4a916e8b398a8674a5177bf3c6b798f
Yelshat Duskaliyev <ura2178@gmail.com>
2020-08-14 17:32:29 +0600

    extended tests coverage

--- time.go
+++ time.go
@@ -9,6 +9,33 @@ import (
 // ISO8601Clock describes time layout in ISO 8601 standard
 const ISO8601Clock = "15:04:05"
 
+// Clock is a wrapper for time.time to allow parsing datetime stamp with time only in
+// ISO 8601 format, like "15:04:05"
+type Clock time.Time
+
+// MarshalJSON marshals time into time
+func (h Clock) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(h).Format(ISO8601Clock))
+}
+
+// UnmarshalJSON converts time to ISO 8601 representation
+func (h *Clock) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	val, ok := v.(string)
+	if !ok {
+		return errors.New("invalid clock")
+	}
+	t, err := time.Parse(ISO8601Clock, val)
+	if err != nil {
+		return err
+	}
+	*h = Clock(t)
+	return nil
+}
+
 // Duration is a wrapper of time.Duration, that allows to marshal and unmarshal time in RFC3339 format
 type Duration time.Duration
 
@@ -37,31 +64,4 @@ func (d *Duration) UnmarshalJSON(b []byte) error {
 	default:
 		return errors.New("invalid duration")
 	}
-}
-
-// Clock is a wrapper for time.time to allow parsing datetime stamp with time only in
-// ISO 8601 format, like "15:04:05"
-type Clock time.Time
-
-// MarshalJSON marshals time into time
-func (h Clock) MarshalJSON() ([]byte, error) {
-	return json.Marshal(time.Time(h).Format(ISO8601Clock))
-}
-
-// UnmarshalJSON converts time to ISO 8601 representation
-func (h *Clock) UnmarshalJSON(b []byte) error {
-	var v interface{}
-	if err := json.Unmarshal(b, &v); err != nil {
-		return err
-	}
-	val, ok := v.(string)
-	if !ok {
-		return errors.New("invalid clock")
-	}
-	t, err := time.Parse(ISO8601Clock, val)
-	if err != nil {
-		return err
-	}
-	*h = Clock(t)
-	return nil
 }
--- time_test.go
+++ time_test.go
@@ -9,39 +9,46 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestHourMinSec_UnmarshalJSON(t *testing.T) {
-	var s struct {
-		T Clock `json:"t"`
-	}
-	err := json.Unmarshal([]byte(`{"t": "19:24:00"}`), &s)
+func TestClock_UnmarshalJSON(t *testing.T) {
+	var c Clock
+	err := c.UnmarshalJSON([]byte("\"19:24:00\""))
 	require.NoError(t, err)
-	assert.Equal(t, Clock(time.Date(0, time.January, 1, 19, 24, 0, 0, time.UTC)), s.T)
+	assert.Equal(t, Clock(time.Date(0, time.January, 1, 19, 24, 0, 0, time.UTC)), c)
 
-	s = struct {
-		T Clock `json:"t"`
-	}{}
-	err = json.Unmarshal([]byte(`{"t": "08:00:00"}`), &s)
-	require.NoError(t, err)
-	assert.Equal(t, Clock(time.Date(0, time.January, 1, 8, 00, 0, 0, time.UTC)), s.T)
+	// errors
+	err = c.UnmarshalJSON([]byte("19:24:00")) // time should be presented as string
+	require.Error(t, err)
+	assert.IsType(t, &json.SyntaxError{}, err, "time should be escaped in quotes")
+
+	err = c.UnmarshalJSON([]byte("32145")) // invalid clock format
+	assert.EqualError(t, err, "invalid clock", "clock should be in format \"15:04:05\"")
+
+	err = c.UnmarshalJSON([]byte("\"19:24:c00\""))
+	require.Error(t, err)
+	assert.IsType(t, &time.ParseError{}, err, "invalid character \"c\" in seconds")
 }
 
 func TestDuration_UnmarshalJSON(t *testing.T) {
-	type temp struct {
-		T Duration `json:"t"`
-	}
-	var s temp
-	err := json.Unmarshal([]byte(`{"t": "1h5m3s"}`), &s)
+	var d Duration
+	err := d.UnmarshalJSON([]byte("\"1h5m3s\""))
 	require.NoError(t, err)
-	assert.Equal(t, Duration(time.Hour+5*time.Minute+3*time.Second), s.T)
+	assert.Equal(t, Duration(time.Hour+5*time.Minute+3*time.Second), d)
 
-	s = temp{}
-	err = json.Unmarshal([]byte(`{"t": 3903000000000}`), &s)
+	err = d.UnmarshalJSON([]byte("3903000000000"))
 	require.NoError(t, err)
-	assert.Equal(t, Duration(time.Hour+5*time.Minute+3*time.Second), s.T)
+	assert.Equal(t, Duration(time.Hour+5*time.Minute+3*time.Second), d)
 
-	s = temp{}
-	err = json.Unmarshal([]byte(`{"t": true}`), &s)
-	assert.Errorf(t, err, "invalid duration")
+	// errors
+	err = d.UnmarshalJSON([]byte("true"))
+	require.EqualError(t, err, "invalid duration", "passed bool to type duration")
+
+	err = d.UnmarshalJSON([]byte("1h5m3s"))
+	require.Error(t, err)
+	assert.IsType(t, &json.SyntaxError{}, err, "duration should be escaped in quotes or passed as integer")
+
+	err = d.UnmarshalJSON([]byte("\"\""))
+	require.Error(t, err)
+	assert.EqualError(t, err, "time: invalid duration ", "passed empty string to time parser")
 }
 
 func TestDuration_MarshalJSON(t *testing.T) {
@@ -50,7 +57,7 @@ func TestDuration_MarshalJSON(t *testing.T) {
 	assert.Equal(t, []byte(`"1h5m3s"`), bytes)
 }
 
-func TestHourMinSec_MarshalJSON(t *testing.T) {
+func TestClock_MarshalJSON(t *testing.T) {
 	bytes, err := Clock(time.Date(0, time.January, 1, 19, 24, 0, 0, time.UTC)).MarshalJSON()
 	require.NoError(t, err)
 	assert.Equal(t, []byte(`"19:24:00"`), bytes)
